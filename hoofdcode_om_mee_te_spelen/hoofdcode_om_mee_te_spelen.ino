6f9344474a8a02df1d49d12e5570d76f5f6e882a
casper <casper@vboxx.nl>
2021-06-08 14:52:44 +0200

    completere versie hoodfcode
    pindefines kloppen niet

--- hoofdcode_om_mee_te_spelen/hoofdcode_om_mee_te_spelen.ino
+++ hoofdcode_om_mee_te_spelen/hoofdcode_om_mee_te_spelen.ino
@@ -1,147 +1,222 @@
-/*
-    Simulatie van plaats s en snelheid v van een massa m en een stuwkracht F
-    Bewegingsvergelijking:
-       m.a = F
+#include <math.h>
 
-    Integratie obv
-       a = dv/dt
-       v = dx/dt
+/* pin defines */
+// motoren
+#define LINKS_PWM_PIN     9
+#define LINKS_VOORUIT     10
+#define LINKS_ACHTERUIT   11
+#define RECHTS_PWM_PIN    12
+#define RECHTS_VOORUIT    13
+#define RECHTS_ACHTERUIT  14
+#define ZIJ_PWM_PIN       15
+#define ZIJ_VOORUIT       16
+#define ZIJ_ACHTERUIT     17
+// Time-of-Flight xshut pins
+#define TOF_VOOR        4
+#define TOF_ZIJ_VOOR    5
+#define TOF_ZIJ_ACHTER  6
+// relaits
+#define HOOFD_RELAIT  7
+#define VENT_RELAIT   8
+// knoppen
+#define NOODSTOP_KNOP 18
 
-    => dv = a.dt => v_nw - v_oud = a.dt => v_nw = v_oud + a.dt =>
-       v = v + a*dt
-       x = x + v*dt
-*/
-const bool simulator = true;
+/* systeem constanten */
+// hovercraft traagheden en afmetingen constante
+#define M 1.216   // massa                          in kg
+#define I 0.024   // traagheidsmoment               in kg.m^2
+#define LE 0.45 // lengte                         in m
+#define BR 0.29 // breedte                        in m
+#define D 0.1355  // afstand motor tot middenlijn   in m
+#define B 0.271   // afstand tussen motoren (2*d)   in m
 
-const float m = 0.500;      // kg
-const float F_max = 1.0;
-const float F_min = -1.0;
-const float bF_max = 1.0;
-const float bF_min = -1.0;
-const float dr_error_som_min = -1.0;
-const float dr_error_som_max = 1.0;
-float alpha;                    // m/s2
-float omega = 0.0, theta = 0.0;     // Beginwaarden
-float dr_error, dr_error_oud, dr_derror, dr_error_som;
-float dr_sp = 90.0;
-float dr_Kp = 1.0;
-float dr_Kd = 2.0;
-float dr_Ki = 30.0;
-float bF = 0.1;
-float I = 0.1;
-float dr_Mz = 0.01;
+// motor constante
+#define MOTOR_LINKS_MAX 0.13734
+#define MOTOR_LINKS_MIN -0.14715
+#define MOTOR_LINKS_MIN_PWM_VOOR 89.0
+#define MOTOR_LINKS_MIN_PWM_ACHTER 77.069
+#define MOTOR_LINKS_VOOR_FUNC(F) (2459.2*F - 89)
+#define MOTOR_LINKS_ACHTER_FUNC(F) (2211.2*F -77.069)
+#define MOTOR_RECHTS_MAX 0.16677
+#define MOTOR_RECHTS_MIN -0.14715
+#define MOTOR_RECHTS_MIN_PWM_VOOR 59.799
+#define MOTOR_RECHTS_MIN_PWM_ACHTER 24.027
+#define MOTOR_RECHTS_VOOR_FUNC(F) (1855.9*F - 59.799)
+#define MOTOR_RECHTS_ACHTER_FUNC(F) (1907.7*F - 24.027)
+#define MOTOR_ZIJ_MAX 0.15
+#define MOTOR_ZIJ_MIN -0.15
+#define MOTOR_ZIJ_MIN_PWM_VOOR 50
+#define MOTOR_ZIJ_MIN_PWM_ACHTER 50
+#define MOTOR_ZIJ_VOOR_FUNC(F) 2000*F - 50
+#define MOTOR_ZIJ_ACHTER_FUNC(F) 2000*F - 50
 
-float F = 1;
-float a;                    // m/s2
-float v = -1.0, x = 310.0;     // Beginwaarden
-float st_error, st_error_oud, st_derror;
-float st_sp = 300;
-float st_Kp = 5.0;
-float st_Kd = 1.0;
+/* state difines */
+#define IDLE_STATE 0
+#define NOODSTOP_STATE 1
 
+int state = IDLE_STATE;
+
+//const bool simulator = true;
+struct Vect {
+  float x;
+  float y;
+};
+
+/* bewegins waardes */
+// lineare domein
+Vect a = {0.0, 0.0};    // versnelling  in m/s^2
+Vect v = {0.0, 0.0};  // snelheid     in m/s
+Vect s = {0.0, 0.0};  // afstand      in m
+//rotatie domein
+float alpha = 0.0;  // hoekversnelling  in rad/s^2
+float omega = 0.0;  // hoeksnelheid     in rad/s
+float theta = 0.0;  // hoek             in rad
+
+/* Cycle variables */
+const float cyclustijd = 100;  // cyclustijd van de superloop  in ms
 long t_oud, t_nw;
 float dt;
-
-// Zorg dat de plot op één pagina past
-const long cyclustijd = 10;           // ms
-const long simulatietijd = 10 * 1000; // ms
-long tEind;
-const int pixels = 500;               // aantal weer te geven punten
-const float tijdPerPixel = simulatietijd * 1.0 / pixels;
-double tijdVoorNwePixelPlot;
+int dt_ms;
 
 void setup() {
   Serial.begin(57600);
-  Serial.print("bF alpha omega theta");  // De legenda
-  if (tijdPerPixel < cyclustijd) Serial.println("__XXXXXX__tijdPerPixel.<.cyclustijd___XXXXXX");
-  else Serial.println();
+
+  /* set pin in-/ output */
+  // outputs
+  pinMode(LINKS_PWM_PIN, OUTPUT);
+  pinMode(LINKS_VOORUIT, OUTPUT);
+  pinMode(LINKS_ACHTERUIT, OUTPUT);
+  pinMode(RECHTS_PWM_PIN, OUTPUT);
+  pinMode(RECHTS_VOORUIT, OUTPUT);
+  pinMode(RECHTS_ACHTERUIT, OUTPUT);
+  pinMode(ZIJ_PWM_PIN, OUTPUT);
+  pinMode(ZIJ_VOORUIT, OUTPUT);
+  pinMode(ZIJ_ACHTERUIT, OUTPUT);
+  
+  pinMode(TOF_VOOR, OUTPUT);
+  pinMode(TOF_ZIJ_VOOR, OUTPUT);
+  pinMode(TOF_ZIJ_ACHTER, OUTPUT);
+  
+  pinMode(HOOFD_RELAIT, OUTPUT);
+  pinMode(VENT_RELAIT, OUTPUT);
+  // inputs
+  pinMode(NOODSTOP_KNOP, INPUT);
+
+  /* Zet pin voor start up */
+  // motoren
+  digitalWrite(LINKS_PWM_PIN, LOW);
+  digitalWrite(LINKS_VOORUIT, LOW);
+  digitalWrite(LINKS_ACHTERUIT, LOW);
+  digitalWrite(RECHTS_PWM_PIN, LOW);
+  digitalWrite(RECHTS_VOORUIT, LOW);
+  digitalWrite(RECHTS_ACHTERUIT, LOW);
+  digitalWrite(ZIJ_PWM_PIN, LOW);
+  digitalWrite(ZIJ_VOORUIT, LOW);
+  digitalWrite(ZIJ_ACHTERUIT, LOW);
+  
+  digitalWrite(TOF_VOOR, HIGH);
+  digitalWrite(TOF_ZIJ_VOOR, HIGH);
+  digitalWrite(TOF_ZIJ_ACHTER, HIGH);
+  
+  digitalWrite(HOOFD_RELAIT, LOW);
+  digitalWrite(VENT_RELAIT, LOW);
 
   t_oud = millis();
-  tijdVoorNwePixelPlot = t_oud;
-  tEind = t_oud + simulatietijd;
-
-  dr_error_oud = dr_sp - theta;
-  st_error_oud = st_sp - x;
 
 }
 
 void loop() {
   t_nw = millis();
-  if (t_nw - t_oud > cyclustijd)
+  dt_ms = t_nw - t_oud;
+  if (dt_ms > cyclustijd)
   {
-    dt = (t_nw - t_oud) * .001;
+    dt = dt_ms * .001;
     t_oud = t_nw;
 
-    if (not simulator)
-    {
+
+    switch (state) {
+      case NOODSTOP_STATE:
+        // noodstop code
+        break;
+      default:
+        // Idle state
+        break;
     }
 
-    //regelaar
-
-    dr_error = dr_sp - theta;
-    dr_derror = dr_error - dr_error_oud;
-    dr_error_som += dr_error;
-    bF = dr_Kp * dr_error + dr_Kd * dr_derror / dt + dr_Ki * dr_error_som * dt;
-    dr_error_som = constrain(dr_error_som, dr_error_som_min, dr_error_som_max);
-    bF = constrain(bF, bF_min, bF_max);
-    dr_error_oud = dr_error;
-
-    st_error = st_sp - x;
-    st_derror = st_error - st_error_oud;
-    F = st_Kp * st_error + st_Kd * st_derror / dt;
-    F = constrain(F, F_min, F_max);
-    st_error_oud = st_error;
-
-
-    if (simulator)
-    {
-
-      alpha = bF / I;
-      omega = omega + alpha * dt;
-      theta = theta + omega * dt;
-
-      a = F / m;      //
-      v = v + a * dt; // deze twee vgln eigenlijk verwisselen van plaats (evt: v += a * dt)
-      x = x + v * dt;
-
-      // Alleen plotten als tijdPerPixel is verlopen
-      if (t_nw > tijdVoorNwePixelPlot) {
-        tijdVoorNwePixelPlot = tijdVoorNwePixelPlot + tijdPerPixel;
-        Serial.print(bF);
-        Serial.print(" ");
-        Serial.print(alpha);
-        Serial.print(" ");
-        Serial.print(omega);
-        Serial.print(" ");
-        Serial.println(dr_error);
-
-//        Serial.print(F);
-//        Serial.print(" ");
-//        Serial.print(a);
-//        Serial.print(" ");
-//        Serial.print(v);
-//        Serial.print(" ");
-//        Serial.println(x);
-      }
-      // Stop met plotten, anders loopt de grafiek van het scherm af
-      while (t_nw > tEind); // Vanglus, einde van het programma
-    }
   }
 }
 
-//void motorsturing(F, bF)  {
-//  Flinks = F / 2 - bF / b;
-//  Frechts = F / 2 + bF / b;
-//  pwm_links = motorfunctie_links();
-//  pwm_rechts = motorfunctie_rechts();
-//}
-//
-//void motorfunctie_links(Flinks) {
-//  if (Flinks > 0) {
-//    pwm_links = avooruit * x + bvooruitlinks;
-//  }
-//  else
-//    (
-//      pwm_links = aaachteruit * x + bachteruitlinks;
-//  }
-//}
+void hoofd_motorsturing(float Fv, float bF, float F_zij)  {
+  
+  float Flinks = Fv / 2 - bF / B;
+  float Frechts = Fv / 2 + bF / B;
+  
+  set_pwm_links(Flinks);
+  set_pwm_rechts(Frechts);
+
+  set_pwm_zij(F_zij);
+}
+
+void set_pwm_links(float F) {
+  // constrain de krachten tot de minimale en maximale van de motor
+  F = constrain(F, MOTOR_LINKS_MIN, MOTOR_LINKS_MAX);
+  // DT van het pwm signaal
+  float pwm = 0;
+  // als de motor vooruit moet
+  if (F > 0) {
+    pwm = MOTOR_LINKS_VOOR_FUNC(F);
+    digitalWrite(LINKS_VOORUIT, HIGH);
+    digitalWrite(LINKS_ACHTERUIT, LOW);
+  // als de motor acter uit moet
+  } else {
+    F = fabs(F);
+    pwm = MOTOR_LINKS_ACHTER_FUNC(F);
+    digitalWrite(LINKS_VOORUIT, LOW);
+    digitalWrite(LINKS_ACHTERUIT, HIGH);
+  }
+  // limiteer het pwm sigaal dus de uiterst mogelijke waarden
+  pwm = constrain(pwm, 0, 254);
+  analogWrite(LINKS_PWM_PIN, int(pwm));
+}
+void set_pwm_rechts(float F) {
+  // constrain de krachten tot de minimale en maximale van de motor
+  F = constrain(F, MOTOR_RECHTS_MIN, MOTOR_RECHTS_MAX);
+  // DT van het pwm signaal
+  float pwm = 0;
+  // als de motor vooruit moet
+  if (F > 0) {
+    pwm = MOTOR_RECHTS_VOOR_FUNC(F);
+    digitalWrite(RECHTS_VOORUIT, HIGH);
+    digitalWrite(RECHTS_ACHTERUIT, LOW);
+  // als de motor acter uit moet
+  } else {
+    F = fabs(F);
+    pwm = MOTOR_RECHTS_ACHTER_FUNC(F);
+    digitalWrite(RECHTS_VOORUIT, LOW);
+    digitalWrite(RECHTS_ACHTERUIT, HIGH);
+  }
+  // limiteer het pwm sigaal dus de uiterst mogelijke waarden
+  pwm = constrain(pwm, 0, 254);
+  analogWrite(RECHTS_PWM_PIN, int(pwm));
+}
+void set_pwm_zij(float F) {
+  // constrain de krachten tot de minimale en maximale van de motor
+  F = constrain(F, MOTOR_ZIJ_MIN, MOTOR_ZIJ_MAX);
+  // DT van het pwm signaal
+  float pwm = 0;
+  // als de motor vooruit moet
+  if (F > 0) {
+    pwm = MOTOR_ZIJ_VOOR_FUNC(F);
+    digitalWrite(ZIJ_VOORUIT, HIGH);
+    digitalWrite(ZIJ_ACHTERUIT, LOW);
+  // als de motor acter uit moet
+  } else {
+    F = fabs(F);
+    pwm = MOTOR_ZIJ_ACHTER_FUNC(F);
+    digitalWrite(ZIJ_VOORUIT, LOW);
+    digitalWrite(ZIJ_ACHTERUIT, HIGH);
+  }
+  // limiteer het pwm sigaal dus de uiterst mogelijke waarden
+  pwm = constrain(pwm, 0, 254);
+  analogWrite(ZIJ_PWM_PIN, int(pwm));
+}
